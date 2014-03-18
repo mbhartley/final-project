d6bf387af8d4210ab580a6b88117b68056dd9d0b
Matt Hartley <mbhartley1490@gmail.com>
2014-03-18 15:44:33 -0400

    instructor registration functioning, with error callback on duplicate username; needs fine-tuning

---
+++ app/scripts/landing-page-view.js
@@ -0,0 +1,4 @@
+/* Begin on the landing page. Instructor user will begin by registering as an instructor. When clicking on 
+the 'Register Now' button, a modal will pop up and the Instructor user will input a new username, password and 
+email address. On clicking 'Save' in the modal, these values will be captured via JQuery and saved to Parse*/
+
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,50 @@
-//$(function(){
+$(function(){
 
-//Parse.initialize("wRZ1tP8WGEzVkIIcDZHWOQyb6hXAHcIe1bYtyxb0", "LsSHeEh1sK899IlTAtzCOlM86oCppYeuCUYIt5oR");
+Parse.initialize("wRZ1tP8WGEzVkIIcDZHWOQyb6hXAHcIe1bYtyxb0", "LsSHeEh1sK899IlTAtzCOlM86oCppYeuCUYIt5oR");
 
-//}
+}),
+
+/*test working!!!
+var TestObject = Parse.Object.extend("TestObject");
+var testObject = new TestObject();
+testObject.save({foo: "bar"}).then(function(object) {
+  alert("yay! it worked");
+});*/
+
+
+$('#save-instructor-registration').click(function() {
+	console.log('RegisterInstructor is firing!');
+	var user = new Parse.User();
+	//capture the value of user inputs from instructor registration modal    
+	var username = $('.js-instructor-username').val();
+	var password = $('.js-instructor-password').val();
+	var email    = $('.js-instructor-email').val();
+    //set user input values as Parse object values
+	user.set('username', username);
+	user.set('password', password);
+	user.set('email', email);
+	//'signUp' must be used in place of 'save'; save will be used for subsequent edits only
+	user.signUp(null, {
+	    success: function(user) {
+	    	//on success, input fields are cleared
+		    $('.js-instructor-username').val('');
+		    $('.js-instructor-password').val('');
+		    $('.js-instructor-email').val('');
+		    $('.js-instr-reg-modal').modal('hide');
+		    alert('You have successfully registered with Platform. Click Okay to continue and log in.')
+
+		},
+
+		error: function(user, error) {
+		    // on error, error alert is thrown; this could be done better
+		    console.log(error);
+		    $('.create-username').addClass('has-error has-feedback');
+		    $('.error-message').append('Sorry,' + ' ' + error.message + '.' + ' ' + 'Please try again.');
+		}
+	});
+})
+
+
+
+
+	
